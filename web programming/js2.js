3c3cda39657b2a9c9ac56112799fcc21c3651e82
Ritesh Pandey <99082361+Ritesh22p1401b@users.noreply.github.com>
2024-10-03 16:48:26 +0530

    js2.js

---
+++ web programming/js2.js
@@ -0,0 +1,12 @@
+function Confirmation() { 
+    var Val = confirm("Do you want to continue ?"); 
+    if (Val == true) { 
+        console.log(" CONTINUED!"); 
+        return true; 
+    } else { 
+        console.log("NOT CONTINUED!"); 
+        return false; 
+    } 
+
+}
+
